280adb628dd9cead16c4cf4198def243fce85eb8
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2018-12-16 16:10:54 +0900

    able to read joystick value.

--- prototype/read_gamepad/main.c
+++ prototype/read_gamepad/main.c
@@ -1,9 +1,14 @@
+/*
+  read gamepad
+  (C) 2018 TANIGUCHI, Kazushige
+ */
+
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <fcntl.h>
 
 #include <errno.h>
 
@@ -12,7 +17,10 @@
 #include <sys/signalfd.h>
 #include <getopt.h>
 
-#define BULK_OF(x) (sizeof(x)/sizeof(x[0]))
+#include <linux/joystick.h>
+
+#define BULK_OF(x)     (sizeof(x)/sizeof(x[0]))
+#define JOYSTICK_PATH  "/dev/input/js0"
 
 static const struct option long_options[] = {
 	{"help",    no_argument, NULL, 'h'},
@@ -23,7 +31,7 @@ static const struct option long_options[] = {
 int main(int argc, char **argv){
 	int ret;
 	sigset_t sigmask;
-	int epollfd = -1, sigfd = -1;
+	int epollfd = -1, sigfd = -1, jsfd = -1;
 	struct epoll_event evs[3];
 	bool bCnt = true;
 
@@ -45,6 +53,13 @@ int main(int argc, char **argv){
 		goto error_exit;
 	}
 
+	jsfd = open(JOYSTICK_PATH, O_RDONLY);
+	if(jsfd == -1){
+		fprintf(stderr, "failed to open joystick fd %s(%d)\n", strerror(errno), errno);
+		ret = 1;
+		goto error_exit;
+	}
+
 	epollfd = epoll_create1(0);
 	if(epollfd == -1){
 		fprintf(stderr, "failed to create epoll fd %s(%d)\n", strerror(errno), errno);
@@ -61,8 +76,18 @@ int main(int argc, char **argv){
 		goto error_exit;
 	}
 
+	evs[0].events  = EPOLLIN;
+	evs[0].data.fd = jsfd;
+	ret = epoll_ctl(epollfd, EPOLL_CTL_ADD, jsfd, &evs[0]);
+	if(ret == -1) {
+		fprintf(stderr, "failed to add jsfd to epoll fd %s(%d)\n", strerror(errno), errno);
+		ret = 1;
+		goto error_exit;
+	}
+
 	while(bCnt){
 		size_t i;
+
 		ret = epoll_wait(epollfd, &evs[0], BULK_OF(evs), -1);
 		if(ret == -1){
 			fprintf(stderr, "failed to wait epoll fd %s(%d)\n", strerror(errno), errno);
@@ -84,7 +109,25 @@ int main(int argc, char **argv){
 					default:
 						break;
 					}
-
+				}
+			}
+			else if(evs[i].data.fd == jsfd){
+				struct js_event jsevent;
+				ret = read(jsfd, &jsevent, sizeof(jsevent));
+				if(ret == -1){
+					fprintf(stderr, "failed to read jpystick %s(%d)\n", strerror(errno), errno);
+					ret = 1;
+					goto error_exit;
+				}
+				switch(jsevent.type){
+				case JS_EVENT_BUTTON:
+					printf("button %u: %d\n", jsevent.number, jsevent.value);
+					break;
+				case JS_EVENT_AXIS:
+					printf("axis %u: %d\n", jsevent.number, jsevent.value);
+					break;
+				default:
+					break;
 				}
 			}
 		}
@@ -93,6 +136,10 @@ int main(int argc, char **argv){
 	ret = 0;
 
 error_exit:
+
+	if(jsfd != -1){
+		close(jsfd);
+	}
 
 	if(sigfd != -1){
 		close(sigfd);
